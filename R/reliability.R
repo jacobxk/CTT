c10401b2585de055b39b867b9da1e16a4d76180a
John T. Willse <willse@uncg.edu>
2008-08-26 00:00:00 +0000

    version 1.0

---
+++ R/reliability.R
@@ -0,0 +1,34 @@
+`reliability` <-
+function(items, itemal=TRUE, NA.Delete=TRUE){
+
+if(!all(apply(items,c(1,2),is.numeric))) { items <- apply(items,c(1,2),as.numeric)
+          warning("Data is not numeric. Data has been coerced to be numeric.")}
+
+if(NA.Delete==FALSE) { items[is.na(items)] <- 0
+                       warning("Missing values or NA values are converted to zeros.")} 
+     
+
+items <- na.omit(items)
+s <- apply(items,2,var)
+N <- ncol(items)
+
+X <- rowSums(items)
+alpha <- (N/(N-1))*(1 - sum(s)/var(X))
+
+if(itemal){
+  alphad <- array(dim=N)
+  pbis <- array(dim=N)
+  for(i in 1:N){
+    Xd <- rowSums(items[,-i])
+    pvalu <- colMeans(items)
+    alphad[i] <- ((N-1)/(N-2))*(1 - sum(s[-i])/var(Xd))
+    pbis[i] <- cor(items[,i],Xd)
+    out <- list(N_item=N,N_person=nrow(items),alpha=alpha, scale.mean=mean(X), scale.sd=sd(X),
+                alpha.if.deleted=alphad, pbis=pbis, item.mean=pvalu)
+  }
+} 
+else out <- list(N_item=N,N_person=nrow(items),alpha=alpha, scale.mean=mean(X), scale.sd=sd(X))
+class(out) <- "reliability"
+out
+}
+
